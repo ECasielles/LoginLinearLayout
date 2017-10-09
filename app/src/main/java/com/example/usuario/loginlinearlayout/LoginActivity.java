5a80676ff1e7682ca82c1cf7537debc412bb188b
ECasielles <flapspoiler@gmail.com>
2017-10-09 14:34:39 +0200

    Added custom styles for checkbox

--- app/src/main/java/com/example/usuario/loginlinearlayout/LoginActivity.java
+++ app/src/main/java/com/example/usuario/loginlinearlayout/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.example.usuario.loginlinearlayout;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+
+/**
+ * Actividad que maneja el alta de usuarios y
+ * el ingreso a la aplicación.
+ *
+ * @author Enrique Casielles Lapeira
+ * @version 1.0
+ * @see android.app.Activity
+ * @see AppCompatActivity
+ */
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView txvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        txvSignUp = (TextView) findViewById(R.id.txvSignUp);
+        txvSignUp.setPaintFlags(txvSignUp.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
+
 }
--- app/src/main/java/com/example/usuario/loginlinearlayout/SplashActivity.java
+++ app/src/main/java/com/example/usuario/loginlinearlayout/SplashActivity.java
@@ -3,6 +3,14 @@ package com.example.usuario.loginlinearlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Actividad pantalla de llegada de la aplicación.
+ *
+ * @author Enrique Casielles Lapeira
+ * @version 1.0
+ * @see android.app.Activity
+ * @see AppCompatActivity
+ */
 public class SplashActivity extends AppCompatActivity {
 
     @Override
